96e83cbc0f3ff077edb64db76b0912c7b0674ad4
sarath.mangalat <sarath.mangalat@gfi.fr>
2018-06-03 09:30:46 +0200

    added default java method example

---
+++ src/main/java/com/examples/java/defaultMethod/Bus.java
@@ -0,0 +1,26 @@
+package com.examples.java.defaultMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Bus {
+
+    public void print(){
+       //Vehicle.super.print();
+
+       /* Vehicle.blowHorn();
+        FourWheeler.super.print();*/
+
+       // System.out.println("I am a BUS");
+
+        List list = new ArrayList();
+
+       // List.class.getSuperc
+
+    }
+
+
+
+
+
+}
---
+++ src/test/java/SomeTestClass.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SomeTestClass {
+
+    @Test
+    public void someSimpleTest() {
+        Assert.assertEquals("123", "123");
+    }}
